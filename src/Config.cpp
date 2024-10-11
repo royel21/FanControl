81b3c371bea130ffb6098db474b8a9e0cdb7b337
royel21 <royelconsoro@hotmail.com>
2024-10-11 09:21:16 -0400

    some fix

--- src/Config.cpp
+++ src/Config.cpp
@@ -18,9 +18,13 @@ void Config::save() {
 }
 void Config::save(const String& dvn, const String& n, const String& p, const String& srv) {
     strncpy(devname, dvn.c_str(), dvn.length());
+    devname[dvn.length()] = '\0';
     strncpy(wifiname, n.c_str(), n.length());
+    wifiname[n.length()] = '\0';
     strncpy(pass, p.c_str(), p.length());
+    pass[p.length()] = '\0';
     strncpy(server, srv.c_str(), srv.length());
+    server[srv.length()] = '\0';
     EEPROM.put(1, *this);
     EEPROM.commit();
 }
